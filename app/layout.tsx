74674cb2627e44331fbf427c9659af71fd3c90c3
WarFiN <rajvirgoenka@gmail.com>
2024-08-26 23:35:42 +0530

    🔧 Update metadata image URL and add /rtp command in basic_commands page

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     siteName: "Bugatti PvP",
     images: [
       {
-        url: "https://images2.imgbox.com/96/e6/BTja0ggL_o.png",
+        url: "https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/bugattipvp.vercel.app/Bugatti%20PvP/A%20public%20Origins%20SMP%20Server%2C%20Choose%20across%20many%20different%20powers%20ranging%20from%20spiders%20to%20vampire%2C%20Grind%2C%20Grow%2C%20Fight.%20Clan%20wars%2C%20Quests%2C%20Bounties%20and%20much%20more/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F606bc2e9-95c6-42f4-a027-7ba68284560f.jpg%3Ftoken%3DuLsu2DuOU8PhO2Z0qosbmubqWPjmYRJ3nU_07QSVn0I%26height%3D675%26width%3D1200%26expires%3D33260694185/og.png",
         width: 1920,
         height: 1080,
       },
