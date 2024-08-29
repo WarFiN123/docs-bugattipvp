22ca677ff32aed7429850a24543d3d8b0e910389
WarFiN <rajvirgoenka@gmail.com>
2024-08-29 15:20:18 +0530

    ✨ Add Particles component to error page.

--- app/error.tsx
+++ app/error.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
+import Particles from "../components/particles";
 
 export default function Failure({
   failure,
@@ -16,7 +17,11 @@ export default function Failure({
   }, [failure]);
 
   return (
-    <div className="min-h-[99vh] px-2 py-8 flex flex-col gap-3 items-start">
+    <div className="min-h-screen flex flex-col gap-3 justify-center items-center px-2 py-8">
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
       <div>
         <h2 className="text-5xl font-bold">Oops!</h2>
         <p className="text-muted-foreground">Something went wrong!</p>
