23d7d697b88e1968aeed62498b7b9f8b1635d2e1
WarFiN <rajvirgoenka@gmail.com>
2024-08-31 18:04:47 +0530

    ✨ Update breadcrumb link to 'Documentation'
    🎨 Add shader gradient background to Home page
    
    📝 Revise text content for Home page to enhance clarity and engagement

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="relative flex sm:min-h-[91vh] min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
+      <ShaderGradientCanvas
+        style={{ position: "absolute", top: 0, zIndex: "-20" }}
+      >
+      <ShaderGradient
+          control="query"
+          urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=15.1&color1=%2373bfc4&color2=%23ff810a&color3=%238da0ce&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.4&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&toggleAxis=false&type=sphere&uAmplitude=3.2&uDensity=0.8&uFrequency=5.5&uSpeed=0.3&uStrength=0.3&uTime=0&wireframe=false"
+      />
+      </ShaderGradientCanvas>
       <div className="z-10 flex flex-col items-center">
         <h1 className="text-3xl font-bold mb-4 sm:text-7xl font-display">
           Bugatti
@@ -16,10 +24,12 @@ export default function Home() {
         <h1 className="text-3xl font-bold mb-4 sm:text-7xl font-display">
           PvP
         </h1>
-        <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground text">
-          A public Origins SMP Server, Choose across many different powers
-          ranging from spiders to vampire, Grind, Grow, Fight. Clan wars,
-          Quests, Bounties and much more<br></br>
+        <p className="mb-8 sm:text-xl max-w-[800px]">
+          Experience the ultimate Minecraft adventure on our public Origins SMP
+          server! With a vast array of unique powers to choose from, including
+          spiders, vampires, and more, you'll embark on a journey of growth,
+          combat, and exploration. Engage in epic clan wars, complete thrilling
+          quests, and claim coveted bounties in a world of endless possibility.<br></br>
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-5">
           <Link
@@ -43,14 +53,6 @@ export default function Home() {
           <Heart className="w-4 h-4 mr-1" /> ~ By unt1tle and darkterrer
         </span>
       </div>
-      <ShaderGradientCanvas
-        style={{ position: "absolute", top: 0, zIndex: "-20" }}
-      >
-        <ShaderGradient
-          control="query"
-          urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=15.1&color1=%2373bfc4&color2=%23ff810a&color3=%238da0ce&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.4&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&toggleAxis=false&type=sphere&uAmplitude=3.2&uDensity=0.8&uFrequency=5.5&uSpeed=0.3&uStrength=0.3&uTime=0&wireframe=false"
-        />
-      </ShaderGradientCanvas>
     </div>
   );
 }
