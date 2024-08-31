9882bd3786ba22fe52e56e3a10b81bbeee6c208c
WarFiN <rajvirgoenka@gmail.com>
2024-08-31 20:27:29 +0530

    🚀 Add Preloading to Home Page and Discord Icon on Login Button and remove unnecessary code

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@
 import { buttonVariants } from "@/components/ui/button";
 import { page_routes } from "@/lib/routes-config";
 import { ShaderGradientCanvas, ShaderGradient } from "shadergradient";
-import { Heart } from "lucide-react";
+import { Canvas } from '@react-three/fiber'
+import { SiDiscord } from "react-icons/si";
+import { Preload } from '@react-three/drei'
+import { Heart, Gamepad2 } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
@@ -12,6 +15,7 @@ export default function Home() {
       <ShaderGradientCanvas
         style={{ position: "absolute", top: 0, zIndex: "-20" }}
       >
+        <Preload all />
         <ShaderGradient
           control="query"
           urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=15.1&color1=%2373bfc4&color2=%23ff810a&color3=%238da0ce&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.4&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&toggleAxis=false&type=sphere&uAmplitude=3.2&uDensity=0.8&uFrequency=5.5&uSpeed=0.3&uStrength=0.3&uTime=0&wireframe=false"
@@ -33,7 +37,7 @@ export default function Home() {
             href={`/docs/${page_routes[0].href}`}
             className={buttonVariants({ className: "px-6", size: "lg" })}
           >
-            Start Playing
+            <Gamepad2 className="mr-2 w-5 h-5" /> Start Playing
           </Link>
           <Link
             href="https://discord.gg/ZewyUmtVSS"
@@ -43,7 +47,7 @@ export default function Home() {
               size: "lg",
             })}
           >
-            Discord
+            <SiDiscord className="mr-3 w-4 h-4" /> Discord
           </Link>
         </div>
         <span className="flex flex-row items-center gap-2 text-zinc-400 text-md mt-7 max-[800px]:mb-12">
