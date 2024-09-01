75c1b0d7db3dd6ef36d3de073fdab770a3cf52a3
WarFiN <rajvirgoenka@gmail.com>
2024-09-01 17:40:35 +0530

    ✨ Update ShaderGradient props in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,43 @@ export default function Home() {
   return (
     <div className="relative flex sm:min-h-[91vh] min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
       <ShaderGradientCanvas
-        style={{ position: "absolute", top: 0, zIndex: "-20" }}
+      pointerEvents="none"
+      eventPrefix="client"
+      className="pointer-events-none"
+        style={{ position: "absolute", PointerEvents: "none", top: 0, zIndex: "-20" }}
       >
         <Preload all />
         <ShaderGradient
-          control="query"
-          urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&brightness=0.8&cAzimuthAngle=270&cDistance=0.5&cPolarAngle=180&cameraZoom=15.1&color1=%2373bfc4&color2=%23ff810a&color3=%238da0ce&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=env&pixelDensity=1&positionX=-0.1&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.4&rotationX=0&rotationY=130&rotationZ=70&shader=defaults&toggleAxis=false&type=sphere&uAmplitude=3.2&uDensity=0.8&uFrequency=5.5&uSpeed=0.3&uStrength=0.3&uTime=0&wireframe=false"
+          control="props"
+          animate="on"
+          cAzimuthAngle={270}
+          cDistance={0.5}
+          cPolarAngle={180}
+          cameraZoom={15.1}
+          color1={"#73bfc4"}
+          color2={"#ff810a"}
+          color3={"#8da0ce"}
+          envPreset="city"
+          frameRate={10}
+          grain="on"
+          lightType="env"
+          positionX={-0.1}
+          positionY={0}
+          positionZ={0}
+          range="disabled"
+          reflection={0.4}
+          rotationX={0}
+          rotationY={130}
+          rotationZ={70}
+          shader="defaults"
+          type="sphere"
+          uAmplitude={3.2}
+          uDensity={0.8}
+          uFrequency={5.5}
+          uSpeed={0.3}
+          uStrength={0.3}
+          uTime={0}
+          wireframe={false}
         />
       </ShaderGradientCanvas>
       <div className="z-10 flex flex-col items-center">
